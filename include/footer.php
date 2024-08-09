0b472422669a03c8ef75f3bba8c55804614b7dad
Afreed007 <afreed0076@gmail.com>
2024-08-09 19:07:41 +0530

    first commit

---
+++ include/footer.php
@@ -0,0 +1,67 @@
+<footer class="full-row bg-secondary p-0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div class="divider py-40">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-4">
+                                    <div class="footer-widget mb-4">
+                                        <!-- <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/blockfernslogo1.jpg" alt="image"></a> </div> -->
+                                        <p class="pb-20 text-white">“At BlockFernsReality, we are dedicated to providing exceptional real estate services to our clients. With years of experience and a deep understanding of the market, we strive to make the buying, selling, and renting process as seamless as possible. Our commitment to integrity, professionalism, and customer satisfaction sets us apart in the industry. Whether you are looking to find your dream home, sell your property, or make a sound investment, our team is here to guide you every step of the way.”</p>
+										
+                                        </div>
+                                </div>
+                                <div class="col-md-12 col-lg-8">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Support</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="#" class="text-white">Forum</a></li>
+                                                    <li><a href="#" class="text-white">Terms and Condition</a></li>
+                                                    <li><a href="#" class="text-white">Frequenlty Asked Question</a></li>
+                                                    <li><a href="contact.php" class="text-white">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Quick Links</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="about.php" class="text-white">About Us</a></li>
+                                                    <li><a href="#" class="text-white">Featured Property</a></li>
+                                                    <li><a href="#" class="text-white">Submit Property</a></li>
+                                                    <li><a href="agent.php" class="text-white">Our Agents</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Contact Us</h4>
+                                                <ul class="text-white">
+                                                    <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>27 Church street ,Bangalore</li>
+                                                    <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i><a href="tel:+91 96006425616"><i class="text-white">+91 96006425616</i></a></li>
+													<li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i><a href="tel:+91 7026423228"><i class="text-white">+91 7026423228</i></a></li>
+                                                    <li class="hover-text-primary"><i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>BlockFernsReality@gmail.com</li>
+                                                    <li class="hover-text-primary"> <i class="fab fa-instagram text-white mr-2 font-13 mt-1"></i><a href="https://www.instagram.com/blockfernsreality?igsh=MTQya2J3eXYzY2NuMQ=="><i class="text-white">BlockFernsReality</i></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row copyright">
+                    <div class="col-sm-6"> <span class="text-white">© <?php echo date('Y');?> Real Estate Website - Developed By Syed Ibrahim & Mohammed Afreed</span> </div>
+                    <div class="col-sm-6">
+                        <ul class="line-menu text-white hover-text-primary float-right">
+                            <li><a href="#">Privacy & Policy</a></li>
+                            <li>|</li>
+                            <li><a href="#"> Site Map</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
